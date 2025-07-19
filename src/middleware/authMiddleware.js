5c13cdc367ff83b7b269e2f2ca015a9a577e72f4
NicolasSanjaya <nicolassanjaya4823@gmail.com>
2025-07-19 23:12:57 +0700

    fix(auth): still cookie

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -6,7 +6,6 @@ exports.authenticateToken = (req, res, next) => {
   // const authHeader = req.headers["authorization"];
   // const token = authHeader && authHeader.split(" ")[1];
   const token = req.cookies?.token;
-  console.log({ token });
 
   if (!token) {
     return res
