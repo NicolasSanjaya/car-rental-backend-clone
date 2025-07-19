a9c1f2cc6496edf1cfefc229da101c93743e23dc
NicolasSanjaya <nicolassanjaya4823@gmail.com>
2025-07-19 16:59:46 +0700

    fix(midtans): change to development mode

--- src/config/midtrans.js
+++ src/config/midtrans.js
@@ -2,7 +2,7 @@ const midtransClient = require("midtrans-client");
 require("dotenv").config();
 
 const snap = new midtransClient.Snap({
-  isProduction: process.env.NODE_ENV === "production",
+  isProduction: false,
   serverKey: process.env.MIDTRANS_SERVER_KEY,
   clientKey: process.env.MIDTRANS_CLIENT_KEY,
 });
